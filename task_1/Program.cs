fbe73118379fdf21f08c015f3a94637fa5a06a05
Aleksandra Popova <alexnika@bk.ru>
2022-05-27 15:04:21 +0300

    added task_1

---
+++ task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// 1.Показать двумерный массив размером m×n заполненный вещественными числами
+
+Console.WriteLine("Введите число n:");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число m:");
+int m = int.Parse(Console.ReadLine());
+
+double [,] myArray = new double [n,m];
+
+for (int row = 0; row < n; row++) 
+{
+  for (int column = 0; column < m; column++)
+  {
+    myArray [row, column] = new Random().NextDouble()* 10; 
+  }
+}
+for (int row = 0; row < myArray.GetLength(0); row++) 
+{
+  for (int column = 0; column < myArray.GetLength(1); column++)
+  {
+      Console.Write($"{Math.Round((myArray[row,column]), 2)}" + "\t");  
+  } 
+  Console.WriteLine(); 
+}
