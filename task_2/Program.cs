a16cb8ae7eb3d9d26f31dd56add12e76e4399eee
Aleksandra Popova <alexnika@bk.ru>
2022-05-27 18:01:09 +0300

    added task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// 2.Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+Console.WriteLine("Введите число n:");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите число m:");
+int m = int.Parse(Console.ReadLine()?? "0");
+Console.WriteLine();
+int [,] myArray = new int [n,m];
+
+for (int row = 0; row < n; row++) 
+{
+  for (int column = 0; column < m; column++)
+  {
+     myArray [row, column] = row + column; 
+     Console.Write($"{myArray [row, column]}" + "\t"); 
+  }
+  Console.WriteLine();
+}
+
+
